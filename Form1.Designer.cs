92b4760a1a3d652cba31c695b891f7a29bc0050a
Your Name <you@example.com>
2024-04-06 20:08:17 +0300

    committed

---
+++ Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Carrier
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Carrier.Properties.Resources.HFS_CVL_Syvaryn;
+            this.pictureBox3.Location = new System.Drawing.Point(120, 311);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(182, 50);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            this.pictureBox3.MouseLeave += new System.EventHandler(this.pictureBox3_MouseLeave);
+            this.pictureBox3.MouseHover += new System.EventHandler(this.pictureBox3_MouseHover);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Carrier.Properties.Resources.target;
+            this.pictureBox2.Location = new System.Drawing.Point(195, 105);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(43, 57);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Carrier.Properties.Resources.HFS_Karan1;
+            this.pictureBox1.Location = new System.Drawing.Point(131, 168);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(171, 53);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseLeave += new System.EventHandler(this.pictureBox1_MouseLeave);
+            this.pictureBox1.MouseHover += new System.EventHandler(this.pictureBox1_MouseHover);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::Carrier.Properties.Resources.target;
+            this.pictureBox4.Location = new System.Drawing.Point(195, 248);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(43, 57);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 50;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1389, 917);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Carrier
+{
+    public partial class Form1 : Form
+    {
+        int Yint;
+        int Xint;
+        int Yint2;
+        int Xint2;
+        bool Syvaryn = false;
+        bool Karan = false;
+        bool clicked1 = false;
+        bool clicked2 = false;
+        public Form1()
+        { 
+            InitializeComponent();
+            this.KeyPreview = true;
+            pictureBox2.Visible = false;
+            pictureBox4.Visible = false;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Karan = true;
+            clicked1 = true;
+            pictureBox1.BorderStyle = BorderStyle.FixedSingle;
+        }
+
+        private void pictureBox1_MouseHover(object sender, EventArgs e)
+        {
+            pictureBox2.Location = new Point(pictureBox1.Location.X+65, pictureBox1.Location.Y-78);
+            pictureBox2.Visible = true;
+        }
+
+        private void pictureBox1_MouseLeave(object sender, EventArgs e)
+        {
+            pictureBox2.Visible = false;
+            pictureBox1.BorderStyle = BorderStyle.None;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (clicked1 == true)
+            {
+                Xint = Cursor.Position.X - 85; 
+                Yint = Cursor.Position.Y - 26;
+            }
+
+            if (clicked2 == true)
+            {
+                Xint2 = Cursor.Position.X - 85;
+                Yint2 = Cursor.Position.Y - 26;
+            }
+
+            clicked1 = false;
+            clicked2 = false;
+
+        }
+
+        public async void timer1_Tick(object sender, EventArgs e)
+        {
+        await Task.Delay(5);
+            if (Karan == true)
+            {
+                if (pictureBox1.Location.X != Xint || pictureBox1.Location.Y != Yint)
+                {
+                if (pictureBox1.Location.X > Xint)
+                {
+                    pictureBox1.Location = new Point(pictureBox1.Location.X - 1, pictureBox1.Location.Y);
+                }
+                else if (pictureBox1.Location.X < Xint)
+                {
+                    pictureBox1.Location = new Point(pictureBox1.Location.X + 1, pictureBox1.Location.Y);
+                }
+
+                if (pictureBox1.Location.Y > Yint)
+                {
+                    pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y-1);
+                }
+                else if (pictureBox1.Location.Y < Yint)
+                {
+                    pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y + 1);
+                }
+                }
+            }
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            Syvaryn = true;
+            clicked2 = true;
+            pictureBox3.BorderStyle = BorderStyle.FixedSingle;
+
+        }
+
+        private void pictureBox3_MouseHover(object sender, EventArgs e)
+        {
+            pictureBox4.Location = new Point(pictureBox3.Location.X + 65, pictureBox3.Location.Y - 78);
+            pictureBox4.Visible = true;
+
+        }
+
+        private void pictureBox3_MouseLeave(object sender, EventArgs e)
+        {
+            pictureBox4.Visible = false;
+            pictureBox3.BorderStyle = BorderStyle.None;
+
+        }
+
+        private async void timer2_Tick(object sender, EventArgs e)
+        {
+            await Task.Delay(5);
+                if (pictureBox3.Location.X != Xint2 || pictureBox3.Location.Y != Yint)
+                {
+                    if (pictureBox3.Location.X > Xint2)
+                    {
+                        pictureBox3.Location = new Point(pictureBox3.Location.X - 1, pictureBox3.Location.Y);
+                    }
+                    else if (pictureBox3.Location.X < Xint2)
+                    {
+                        pictureBox3.Location = new Point(pictureBox3.Location.X + 1, pictureBox3.Location.Y);
+                    }
+
+                    if (pictureBox3.Location.Y > Yint2)
+                    {
+                        pictureBox3.Location = new Point(pictureBox3.Location.X, pictureBox3.Location.Y - 1);
+                    }
+                    else if (pictureBox3.Location.Y < Yint2)
+                    {
+                        pictureBox3.Location = new Point(pictureBox3.Location.X, pictureBox3.Location.Y + 1);
+                    }
+                }
+        }
+    }
+}
